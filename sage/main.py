ccfb86a17f95d997be12cb750a5f9b1ed922628d
kfike <kate.e.fike@gmail.com>
2022-06-25 21:30:14 -0400

    Clean up imports

--- sage/main.py
+++ sage/main.py
@@ -7,9 +7,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-import base64
-from bs4 import BeautifulSoup
-import re
+import email_parser
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -46,51 +44,21 @@ def main():
             print('No messages found.')
             return
 
+        # TODO: Perform a partial synchronization once the history of message IDs is stored
         message_ids = results['messages']
         # Get the message details
         messages = [service.users().messages().get(userId='me', id=msg_id['id']).execute() for msg_id in message_ids]
 
-        for msg in messages:
-            headers = msg['payload']['headers']
-            
-            for data in headers:
-                if data['name'] == 'From':
-                    sender = data['value']
-                if data['name'] == 'Subject':
-                    subject = data['value']
-            
-            if sender == 'Chase <no.reply.alerts@chase.com>':
-                pass
-                # print(subject)
+        success = email_parser.main(messages)
 
-            str_data = None
-            if "data" in msg['payload']['body']:
-                str_data = msg['payload']['body']['data']
-                html_data = data_encoder(str_data)
-            elif "data" in msg['payload']['parts'][0]['body']:
-                str_data = msg['payload']['parts'][0]['body']['data']
-                html_data = data_encoder(str_data)
-            else:
-                print("The payload body has no data.")
-
-            if html_data:
-                soup = BeautifulSoup(html_data, 'lxml')
-                for elem in soup(text=re.compile(r' [$]\d\d[.]\d\d')):
-                    print(subject)
-                    # print(elem.parent)
-
-
+        if success:
+            print("SUCCESS")
+            exit
 
     except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
+        # TODO: Handle errors from gmail API
         print(f'An error occurred: {error}')
 
-
-def data_encoder(str_data):
-    if len(str_data)>0:
-        byte_data = base64.urlsafe_b64decode(str_data)
-        html_data = str(byte_data, 'utf-8')
-    return html_data
 
 if __name__ == '__main__':
     main()
