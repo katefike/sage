ceb47cfda214cff034c676bc32e917b097e12fca
kfike <kate.e.fike@gmail.com>
2022-09-12 09:46:47 -0400

    Add all variations to host and domain name to mydestination
    Emails are successfully receiving now

--- scripts/postfix_config.sh
+++ scripts/postfix_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # postconf -e "home_mailbox = Maildir/"
-postconf -e "mynetworks = 24.140.68.119, 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128, localhost"
+postconf -e "mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128, localhost"
 postconf -e "smtpd_relay_restrictions = reject"
 postconf -e "smtpd_recipient_restrictions = permit_mynetworks, permit_sasl_authenticated"
+postconf -e "alias_maps = hash:/etc/aliases"
+postconf -e "mydestination = $myhostname, example.com, localhost.example.com, localhost.localdomain, localhost"
