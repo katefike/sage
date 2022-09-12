75d633fbbb360573fd6ce760ac10ed68c6e3cb76
kfike <kate.e.fike@gmail.com>
2022-09-11 20:44:26 -0400

    Kind of resolves 'Relay Access Denied' error; yields new 'Recipient Address Rejected' error

--- scripts/postfix_config.sh
+++ scripts/postfix_config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-postconf -e "home_mailbox = Maildir/"
-apt-get install mailutils
+# postconf -e "home_mailbox = Maildir/"
+postconf -e "mynetworks = 24.140.68.119, 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128, localhost"
+postconf -e "smtpd_relay_restrictions = reject"
+postconf -e "smtpd_recipient_restrictions = permit_mynetworks, permit_sasl_authenticated"
