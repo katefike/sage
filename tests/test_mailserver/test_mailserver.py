b0949a4dd55be235a62ecc11b2392a8333976ece
kfike <kate.e.fike@gmail.com>
2022-12-18 21:53:13 -0500

    Change fixture name

--- tests/test_mailserver/test_mailserver.py
+++ tests/test_mailserver/test_mailserver.py
@@ -10,8 +10,8 @@ def test_sending_single_email(send_single_email):
     assert send_single_email is True
 
 
-def test_getting_emails(get_emails):
+def test_getting_emails(get_all_emails):
     """
     Test if emails can be retrieved via IMAP.
     """
-    assert len(get_emails) is not 0
+    assert len(get_all_emails) is not 0
