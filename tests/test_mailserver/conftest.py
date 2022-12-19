19caeb20a1603d46ef175375dfc824bbed4ec83a
kfike <kate.e.fike@gmail.com>
2022-12-18 22:06:16 -0500

    Add top level confest for initializing 'globals'

--- tests/test_mailserver/conftest.py
+++ tests/test_mailserver/conftest.py
@@ -2,27 +2,12 @@
 # This file is run during the test collection phase (before any tests are run)
 
 import datetime
-import os
-import pathlib
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 import imap_tools
 import pytest
-from dotenv import load_dotenv
-
-
-def pytest_configure():
-    app_root = str(pathlib.Path(__file__).parent.parent.parent)
-    env_path = app_root + "/.env"
-    if not load_dotenv(env_path):
-        print(f".env faled to load from {env_path}")
-    pytest.DOMAIN = os.environ.get("DOMAIN")
-    pytest.IMAP4_FQDN = os.environ.get("IMAP4_FQDN")
-    pytest.FORWARDING_EMAIL = os.environ.get("FORWARDING_EMAIL")
-    pytest.RECEIVING_EMAIL_USER = os.environ.get("RECEIVING_EMAIL_USER")
-    pytest.RECEIVING_EMAIL_PASSWORD = os.environ.get("RECEIVING_EMAIL_PASSWORD")
 
 
 @pytest.fixture()
