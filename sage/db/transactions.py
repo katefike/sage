bd600e6afb27958a62e11c10d50590dd6df3294d
kfike <kate.e.fike@gmail.com>
2023-01-03 22:31:12 -0500

    Update statements to use transaction object

--- sage/db/transactions.py
+++ sage/db/transactions.py
@@ -1,75 +1,75 @@
 """
 1. Get bank id
 2. Get entity id
-3. Convert amount to cents
 4. Insert transaction
 """
-from contextlib import closing
-from loguru import logger
-from db import open_connection
 from typing import Dict, Optional
-import execute_statements
+
+from loguru import logger
+
+from sage.db.execute_statements import execute_insert, execute_select
+from sage.email_data.transaction import Transaction
+
+logger.add(sink="debug.log")
 
 
-def insert_transaction(parsed_email: Dict) -> bool:
-    bank_id = get_bank_id(parsed_email.get("bank"), parsed_email.get("account"))
-    # entity_id = get_entity_id(parsed_email.get("merchent"), parsed_email.get("payer"))
+def insert_transaction(transaction: Transaction) -> bool:
+    bank_id = get_bank_id(transaction.bank, transaction.account)
     # TODO: Build out entity ID table
     # TODO: Incorporate the entity ID into the transaction
-    gmail_id = parsed_email.get("gmail ID")
-    gmail_time = parsed_email.get("gmail time")
-    descr = parsed_email.get("descr")
-    cent_amount = parsed_email.get("amount")
-
-    transaction_data = (gmail_id, bank_id, cent_amount, gmail_time, descr)
-    insert_stmt = """
+    transaction_data = (
+        transaction.uid,
+        transaction.date,
+        bank_id,
+        transaction.type_,
+        transaction.amount,
+    )
+    stmt = """
     INSERT INTO
-        transactions (gmail_id, bank_id, cents, date, descr)
+        transactions (uid, date, type, bank_id, amount)
     VALUES
-        (?, ?, ?, ?, ?);
+        (%s, %s, %s, %s, %s);
     """
-    success = execute_statements.execute_insert(insert_stmt, transaction_data)
-    if success:
-        return True
+    row_count = execute_insert(stmt, transaction_data)
+    return row_count
 
 
 def get_bank_id(bank_name: str, account: Optional[str]) -> int:
     if account:
-        parameters = (bank_name, account)
-        select_stmt = """
+        params = (bank_name, account)
+        query = """
         SELECT
             id
         FROM
             banks
         WHERE
-            name = ?
-            AND account = ?
+            name = %s
+            AND account = %s
         """
     else:
-        parameters = (bank_name,)
-        select_stmt = """
+        params = (bank_name,)
+        query = """
             SELECT
                 id
             FROM
                 banks
             WHERE
-                name = ?
+                name = %s
             """
-    bank_id_result = execute_statements.execute_select(select_stmt, parameters)
+    bank_id_result = execute_select(query, params)
     if bank_id_result:
         if len(bank_id_result) == 1:
             bank_id = bank_id_result[0]
             return bank_id
         else:
             logger.error(
-                f"{len(bank_id_result)} bank IDs were returned for the bank {bank_name} and account {account}"
+                f"{len(bank_id_result)} bank IDs were returned.\
+                     BANK: {bank_name} ACCOUNT: {account}."
             )
-            return False
     else:
         logger.error(
-            f"No bank IDs were returned for the bank {bank_name} and account {account}"
+            f"No bank IDs were returned. BANK: {bank_name} ACCOUNT: {account}."
         )
-        return False
 
 
 def get_entity_id(entity_name: str) -> int:
@@ -79,7 +79,11 @@ def get_entity_id(entity_name: str) -> int:
     FROM
         entities
     WHERE
-        name = ?
+        name = %s
     """
-    entity_id = execute_statements.execute_select(select_stmt, entity_name)
+    entity_id = execute_select(select_stmt, entity_name)
     return entity_id
+
+
+def insert_entity(entity_name: str):
+    return
