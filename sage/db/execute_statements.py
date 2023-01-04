a9251540a1e8404c6440ffedf8129db493938786
kfike <kate.e.fike@gmail.com>
2023-01-03 21:14:27 -0500

    Use psycopg2

--- sage/db/execute_statements.py
+++ sage/db/execute_statements.py
@@ -1,29 +1,71 @@
+import os
+import pathlib
+from typing import Optional
+
 import psycopg2
+from dotenv import load_dotenv
+from loguru import logger
 
-DB_PATH = pathlib.Path(__file__).parent.parent.parent / "sage_db.db"
+logger.add(sink="debug.log")
+
+# TODO: Consider making a database class with methods
+# https://hackersandslackers.com/psycopg2-postgres-python/
 
 
-def open_connection() -> sqlite3.Connection:
-    return sqlite3.connect(DB_PATH)
+def open_connection():
+    # Get all environment variables
+    app_root = str(pathlib.Path(__file__).parent.parent.parent)
+    env_path = app_root + "/.env"
+    if not load_dotenv(env_path):
+        logger.critical(
+            f"ENVIRONMENT ERROR: .env failed to load from \
+            {env_path}"
+        )
+    POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+    POSTGRES_DB = os.environ.get("POSTGRES_DB")
+    POSTGRES_USER = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    try:
+        conn = psycopg2.connect(
+            host=POSTGRES_HOST,
+            database=POSTGRES_DB,
+            user=POSTGRES_USER,
+            password=POSTGRES_PASSWORD,
+        )
+        return conn.cursor()
+    except psycopg2.DatabaseError as error:
+        logger.critical(f"Failed to connect to the database: {error}")
+        raise error
 
 
-def execute_select(select_stmt: str, parameters: tuple) -> tuple:
+def execute_select(query: str, params: Optional[tuple]) -> tuple:
     with open_connection() as connection:
-        # possibly not needed to close connection
-        with closing(connection.cursor()) as cursor:
+        with connection.cursor() as cursor:
             try:
-                result = cursor.execute(select_stmt, parameters)
-                return result.fetchone()
-            except Error as error:
-                logger.critical(f"{error}")
-                return False
+                if params:
+                    cursor.execute(query, params)
+                else:
+                    cursor.execute(query)
+                records = [row for row in cursor.fetchall()]
+                connection.close()
+                return records
+            except psycopg2.DatabaseError as error:
+                logger.error(
+                    f"Query execution failed due to an error:\
+                        {error}"
+                )
 
 
-def execute_insert(insert_stmt: str, transaction_data: tuple) -> int:
+def execute_insert(stmt: str, data: tuple) -> tuple:
     with open_connection() as connection:
-        with closing(connection.cursor()) as cursor:
+        with connection.cursor() as cursor:
             try:
-                return cursor.execute(insert_stmt, transaction_data)
-            except Error as error:
-                logger.critical(f"{error}")
-                return False
+                cursor.execute(stmt, data)
+                connection.commit()
+                connection.close()
+                return cursor.rowcount
+            except psycopg2.DatabaseError as error:
+                logger.error(
+                    f"Query execution failed due to an error:\
+                     {error}"
+                )
