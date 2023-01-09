9b55ddc1579c69ee3590255bcfa6ac16768f0eea
Kate <81718723+katefike@users.noreply.github.com>
2023-01-08 20:23:48 -0500

    Intentionally fail test

--- tests/test_actions/test_actions.py
+++ tests/test_actions/test_actions.py
@@ -4,4 +4,4 @@ import pytest
 def test_get_receiving_email_user():
     receiving_email_user = pytest.RECEIVING_EMAIL_USER
     print(receiving_email_user)
-    assert "incoming" == receiving_email_user
+    assert "incomin" == receiving_email_user
