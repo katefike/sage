76db72971d3d621f100fd108fc9c997166d2eb76
kfike <kate.e.fike@gmail.com>
2023-01-10 22:22:13 -0500

    Remove tests used for debugging

--- tests/test_actions/test_actions.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-def test_get_receiving_email_user():
-    receiving_email_user = pytest.RECEIVING_EMAIL_USER
-    print(receiving_email_user)
-    assert "incoming" == receiving_email_user
