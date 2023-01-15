8c72f2e17a57c45e3dec4d6b54a8e10b13efc047
kfike <kate.e.fike@gmail.com>
2023-01-14 19:26:31 -0500

    Add debugging test

---
+++ tests/test_action/test_action.py
@@ -0,0 +1,17 @@
+def test_tables(conn):
+    with conn, conn.cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT
+                table_name
+            FROM
+                information_schema.tables
+            WHERE
+                table_schema = 'public'
+                AND table_type = 'BASE TABLE'
+            """
+        )
+        for result in cursor.fetchall():
+            table_name = result[0]
+            print(f"FOUND {table_name}")
+        assert 0 == 1
