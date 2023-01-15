3c5fe0fe9fd02fa773fda29d85a7ea9eab2a5d9d
kfike <kate.e.fike@gmail.com>
2023-01-14 23:33:13 -0500

    Fix expected number of rejected emails

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -9,4 +9,4 @@ def test_rejected_email(delete_all_emails, send_non_transaction_email):
     """Send an email that isn't from the forwarding email"""
     msg_count = main()
     assert msg_count.get("retrieved") == 1
-    assert msg_count.get("rejected") == 0
+    assert msg_count.get("rejected") == 1
