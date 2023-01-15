e764e2d51bc8bba8b74fb9d0613f6fb1d047d713
kfike <kate.e.fike@gmail.com>
2023-01-15 10:26:14 -0500

    Send an email that won't be retrieved

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -5,8 +5,22 @@ Tests the entrypoint of the program, __main__.py
 """
 
 
-def test_rejected_email(delete_all_emails, send_non_transaction_email):
-    """Send an email that isn't from the forwarding email"""
+def test_unretrieved_email(delete_all_emails, send_email):
+    """
+    Send an email that isn't from the forwarding email.
+    It should not be retrieved from the inbox.
+    """
+    html_body = """\
+    <html>
+    <head></head>
+    <body>
+        <p>Hi!<br>
+        This is a single test email.
+        </p>
+    </body>
+    </html>
+    """
+    sender = "test.rejected.email@aol.com"
+    send_email(html_body, sender)
     msg_count = main()
-    assert msg_count.get("retrieved") == 1
-    assert msg_count.get("rejected") == 1
+    assert msg_count.get("retrieved") == 0
