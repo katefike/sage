05caea6f836b1fb031fd1d22aef95f64a9bcd9bd
kfike <kate.e.fike@gmail.com>
2023-01-16 23:13:32 -0500

    Remove assert in fixture

--- tests/test_email_parser/conftest.py
+++ tests/test_email_parser/conftest.py
@@ -4,9 +4,5 @@ from .. import ENV
 
 
 @pytest.fixture(autouse=True)
-def prepare_inbox(fresh_inbox, email_count):
+def prepare_inbox(fresh_inbox):
     fresh_inbox("transaction_emails_development.mbox")
-    count = len(email_count)
-    assert count == 24, print(
-        f"CRITICAL: The transaction emails were not successfully loaded into the inbox. The email count is {count}."
-    )
