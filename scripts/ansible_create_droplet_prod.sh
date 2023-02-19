5a25f059781ac36049b7fd8d181c5d42aa9219ac
Kate Fike <kfike@pop-os.localdomain>
2023-02-18 19:40:47 -0500

    Rename production to prod

--- scripts/ansible_create_droplet_prod.sh
+++ scripts/ansible_create_droplet_prod.sh
@@ -13,4 +13,4 @@ set +o allexport
 
 # Run Ansible Playbook for creating an ephemeral DO Droplet server
 cd ansible/
-exec ansible-playbook -i inventory_production.yml create_droplet_production.yml --ask-become-pass
+exec ansible-playbook -i inventory_prod.yml create_droplet_prod.yml --ask-become-pass
