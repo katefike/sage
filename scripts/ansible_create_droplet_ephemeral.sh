79715b3de6655cb3fff8f3b32acaf901dc27209d
Kate Fike <kfike@pop-os.localdomain>
2023-02-18 19:20:58 -0500

    Define the project root

--- scripts/ansible_create_droplet_ephemeral.sh
+++ scripts/ansible_create_droplet_ephemeral.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# Make the project root pwd
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $__dir/..
+
 set -o nounset -o pipefail -o errexit
 
 # Load all variables from .env and export them all for Ansible to read
 set -o allexport
-cd ..
 source ".env"
 set +o allexport
 
--- scripts/ansible_create_droplet_production.sh
+++ scripts/ansible_create_droplet_production.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# Make the project root pwd
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $__dir/..
+
 set -o nounset -o pipefail -o errexit
 
 # Load all variables from .env and export them all for Ansible to read
 set -o allexport
-cd ..
-source ".env"
+source "/.env"
 set +o allexport
 
 # Run Ansible Playbook for creating an ephemeral DO Droplet server
