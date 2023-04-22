acb897658c06f90d74c6824c1190a0f4f004e3df
Kate Fike <kfike@pop-os.localdomain>
2023-04-22 17:29:24 -0400

    Get droplet id

--- server/digital_ocean_api/__main__.py
+++ server/digital_ocean_api/__main__.py
@@ -1,4 +1,6 @@
 import requests
+from requests.exceptions import HTTPError
+
 from loguru import logger
 
 from . import ENV
@@ -6,22 +8,34 @@ from . import ENV
 logger.add(sink="debug.log", level="INFO")
 
 DO_API_BASE_URL = ENV["DO_API_BASE_URL"]
-DO_API_TOKEN = ENV["DO_API_TOKEN"]
+HEADERS = {"Authorization": f"Bearer {ENV['DO_API_TOKEN']}"}
 
 
 def main():
-    response = get_list_all_droplets()
+    droplet_id = get_droplet_id()
     # TODO: Get droplet with target name
     # droplet name and firewall name needs parameratized
-    droplet_id = 1111111
     response = post_add_droplet_to_firewall(droplet_id)
 
 
-def get_list_all_droplets():
-    requests.get("https://api.github.com")
+def get_droplet_id() -> int:
+    try:
+        url = DO_API_BASE_URL + "droplets?name=sage.ephem"
+        response = requests.get(url, headers=HEADERS)
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        logger.critical(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        logger.critical(f"Other error occurred: {err}")
+    else:
+        json_response = response.json()
+        droplets = json_response.get("droplets")
+        droplet = droplets[0]
+        return droplet.get("id")
 
 
-def post_add_droplet_to_firewall():
+def post_add_droplet_to_firewall(droplet_id: int):
     pass
 
 
