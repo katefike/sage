eefda9bfb36219863f492c6332259e67207f596a
Kate Fike <kfike@pop-os.localdomain>
2023-04-22 18:11:52 -0400

    Get firewall ID

--- server/digital_ocean_api/__main__.py
+++ server/digital_ocean_api/__main__.py
@@ -15,7 +15,8 @@ def main():
     droplet_id = get_droplet_id()
     # TODO: Get droplet with target name
     # droplet name and firewall name needs parameratized
-    response = post_add_droplet_to_firewall(droplet_id)
+    firewall_id = get_firewall_id()
+    response = add_droplet_to_firewall(droplet_id, firewall_id)
 
 
 def get_droplet_id() -> int:
@@ -35,8 +36,27 @@ def get_droplet_id() -> int:
         return droplet.get("id")
 
 
-def post_add_droplet_to_firewall(droplet_id: int):
-    pass
+def get_firewall_id() -> str:
+    try:
+        url = DO_API_BASE_URL + "firewalls"
+        response = requests.get(url, headers=HEADERS)
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        logger.critical(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        logger.critical(f"Other error occurred: {err}")
+    else:
+        json_response = response.json()
+        firewalls = json_response.get("firewalls")
+        for firewall in firewalls:
+            if firewall.get("name") == "sage.test":
+                return firewall.get("id")
+
+
+def add_droplet_to_firewall(droplet_id: int, firewall_id: str):
+    print(droplet_id)
+    print(firewall_id)
 
 
 if __name__ == "__main__":  # pragma: no cover
