3b1b2d851f908a506fcdc1d22c06d116c5be2200
kfike <kate.e.fike@gmail.com>
2023-04-30 18:49:20 -0400

    Fix cURL requests 500 error and 422 error

--- scripts/start_server/__main__.py
+++ scripts/start_server/__main__.py
@@ -19,28 +19,27 @@ APP_ROOT = str(pathlib.Path(__file__).parent.parent.parent)
 
 
 def main():
-    # create_account_ssh_keys()
+    create_account_ssh_keys()
     droplet_id = create_droplet()
-    firewall_id = get_firewall_id()
+    firewall_id = create_firewall(droplet_id)
     add_droplet_to_firewall(droplet_id, firewall_id)
 
 
 def create_account_ssh_keys():
-    # FIXME: This is broken. Returns 422 HTTP error
+    # TODO: If this is being used for the ephemeral server, if the key already exists, don't create a new one
     try:
         url = DO_API_BASE_URL + "account/keys"
         body = {"public_key": f"{EPHEM_SSH_KEY_PUB}", "name": "Sage Local"}
-        print(HEADERS)
-        print(body)
         response = requests.post(url, headers=HEADERS, json=body)
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
     except HTTPError as e:
-        logger.critical(f"HTTP error occurred: {e}")
+        logger.critical(f"HTTP error occurred: {e.response.text}")
     except Exception as e:
         logger.critical(f"Other error occurred: {e}")
     else:
         json_response = response.json()
+        # TODO: Get id
 
 
 def create_droplet() -> int:
@@ -51,13 +50,14 @@ def create_droplet() -> int:
             "region": "nyc1",
             "size": "s-1vcpu-1gb",
             "image": "ubuntu-22-04-x64",
+            # FIXME: Add ssh key when create_account_ssh_keys gets fixed
             # "ssh_keys": [289794, "3b:16:e4:bf:8b:00:8b:b8:59:8c:a9:d3:f0:19:fa:45"],
         }
         response = requests.post(url, headers=HEADERS, json=body)
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
     except HTTPError as e:
-        logger.critical(f"HTTP error occurred: {e}")
+        logger.critical(f"HTTP error occurred: {e.response.text}")
     except Exception as e:
         logger.critical(f"Other error occurred: {e}")
     else:
@@ -70,26 +70,48 @@ def create_droplet() -> int:
         return droplet.get("id")
 
 
-def create_firewall():
+def create_firewall(droplet_id):
     try:
         url = DO_API_BASE_URL + "firewalls"
         body = {
-            "name": "firewall",
-            "inbound_rules": [],
-            "outbound_rules": [],
-            "droplet_ids": [8043964],
+            "name": NAME,
+            "inbound_rules": [
+                {
+                    "protocol": "tcp",
+                    "ports": "22",
+                    "sources": {"addresses": ["0.0.0.0/0", "::/0"]},
+                },
+                {
+                    "protocol": "tcp",
+                    "ports": "80",
+                    "sources": {"addresses": ["0.0.0.0/0", "::/0"]},
+                },
+                {
+                    "protocol": "tcp",
+                    "ports": "22",
+                    "sources": {"addresses": ["0.0.0.0/0", "::/0"]},
+                },
+            ],
+            "outbound_rules": [
+                {
+                    "protocol": "tcp",
+                    "ports": "587",
+                    "destinations": {"addresses": ["0.0.0.0/0", "::/0"]},
+                },
+            ],
+            # "droplet_ids": [droplet_id],
         }
         response = requests.post(url, headers=HEADERS, json=body)
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
     except HTTPError as e:
-        logger.critical(f"HTTP error occurred: {e}")
+        logger.critical(f"HTTP error occurred: {e.response.text}")
     except Exception as e:
         logger.critical(f"Other error occurred: {e}")
     else:
-        if response.status_code != 204:
+        if response.status_code != 202:
             logger.critical(
-                f"Response code other than 204 was returned: {response.status_code}"
+                f"Response code other than 202 was returned: {response.status_code}"
             )
 
 
@@ -112,6 +134,7 @@ def get_droplet_id() -> int:
 
 
 def get_firewall_id() -> str:
+    # This isn't being used and can probably be deleted
     try:
         url = DO_API_BASE_URL + "firewalls"
         response = requests.get(url, headers=HEADERS)
