fec073b380a2292668721dccb179d9de1d4c78ef
kfike <kate.e.fike@gmail.com>
2023-04-30 20:08:01 -0400

    Add password back in

--- scripts/ansible_create_droplet_ephem.sh
+++ scripts/ansible_create_droplet_ephem.sh
@@ -13,5 +13,4 @@ set +o allexport
 
 # Run Ansible Playbook for creating an ephemeral DO Droplet server
 cd server/
-exec ansible-playbook -i ansible/inventory_ephem.yml ansible/create_droplet_ephem.yml 
-# --ask-become-pass
+exec ansible-playbook -i ansible/inventory_ephem.yml ansible/create_droplet_ephem.yml --ask-become-pass
