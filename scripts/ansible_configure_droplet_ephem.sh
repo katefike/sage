b078b38969d182fa6d17af6902d18819ccacd9a3
kfike <kate.e.fike@gmail.com>
2023-06-08 14:56:10 -0400

    Remove port 53 for inbound

--- scripts/ansible_configure_droplet_ephem.sh
+++ scripts/ansible_configure_droplet_ephem.sh
@@ -13,4 +13,4 @@ set +o allexport
 
 # Run Ansible Playbook for creating an ephemeral DO Droplet server
 cd server/
-exec ansible-playbook -i ansible/inventory_ephem.yml -i ansible/imported_playbooks/droplet_hosts ansible/imported_playbooks/configure_droplet_ephem.yml --ask-become-pass
+exec ansible-playbook -i ansible/inventory_ephem.yml -i ansible/imported_playbooks/droplet_hosts ansible/imported_playbooks/configure_droplet_ephem.yml -vvv
