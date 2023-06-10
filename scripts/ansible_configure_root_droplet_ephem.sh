018a370526e22727e94c71171e0215941b1d05fd
kfike <kate.e.fike@gmail.com>
2023-06-10 13:38:04 -0400

    Split configuration as root and configuration as regular user into separate playbooks

--- scripts/ansible_configure_root_droplet_ephem.sh
+++ scripts/ansible_configure_root_droplet_ephem.sh
@@ -11,6 +11,6 @@ set -o allexport
 source ".env"
 set +o allexport
 
-# Run Ansible Playbook for creating an ephemeral DO Droplet server
+# Run Ansible Playbook for configuration as root on the DO Droplet server
 cd server/
-exec ansible-playbook -i ansible/inventory_ephem.yml -i ansible/imported_playbooks/droplet_hosts ansible/imported_playbooks/configure_droplet_ephem.yml
+exec ansible-playbook -i ansible/inventory_ephem.yml -i ansible/imported_playbooks/droplet_hosts ansible/imported_playbooks/configure_root_droplet_ephem.yml
