afb38be8dd6da8cf6f749d13078b686f8fad57b5
kfike <kate.e.fike@gmail.com>
2023-08-20 18:02:04 -0400

    Initial commit

--- tests/test_db/test_db_emails.py
+++ tests/test_db/test_db_emails.py
@@ -1,16 +1,26 @@
 """
 
 """
-import pytest
-
 from sage.__main__ import main
 from tests import utils
 
 
-def test_email_insert():
+def test_email_insert(conn):
     """
     Ensure that all emails in the inbox are inserted into the emails table.
     """
     utils.fresh_inbox("transaction_emails.mbox")
     msg_count = main()
-    assert msg_count.get("retrieved") == msg_count.get("unparsed")
+    # Query to get the count of the emails table.
+    with conn, conn.cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT
+                COUNT(*)
+            FROM
+                emails
+            """
+        )
+        for result in cursor.fetchall():
+            inserted_count = result[0]
+    assert msg_count.get("retrieved") == inserted_count
