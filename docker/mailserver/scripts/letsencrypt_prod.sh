fd1d2401b68c4e54a42b988a2556a1f3409bbf7e
kfike <kate.e.fike@gmail.com>
2023-09-04 22:50:48 -0400

    Delete original letsencrypt script

--- docker/mailserver/scripts/letsencrypt_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Get TLS certs
-echo "Generating TLS certs..."
-certbot certonly --dry-run --non-interactive --standalone --agree-tos -m ${FORWARDING_EMAIL} -d ${HOST}.${DOMAIN}
