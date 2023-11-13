7d4ad64e2b515499902a25e0f66472a46e2d07d8
Kate <81718723+katefike@users.noreply.github.com>
2023-11-12 22:40:00 -0600

    108 fix postfix main.cf tls file paths (#109)
    * Edit comment
    
    * Edit prod main.cf regardless of if TLS/SSL files exist
    
    * Fix postfix config script file name
    
    ---------
    
    Co-authored-by: Kate Fike <kfike@pop-os.localdomain>

--- docker/mailserver/scripts/postfix_dovecot_config_prod.sh
+++ docker/mailserver/scripts/postfix_dovecot_config_prod.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-# Configures /etc/postfix/main.conf
-# and /etc/dovecot/dovecot.conf for production
+# Configures /etc/postfix/main.cf for production
 
 # Here are the docs on the postconf arguments: https://www.postfix.org/postconf.1.html
 CRT_FILE=/etc/letsencrypt/live/prod.${DOMAIN,,}/fullchain.pem
 KEY_FILE=/etc/letsencrypt/live/prod.${DOMAIN,,}/privkey.pem
 
-if [[ -f "${CRT_FILE}" && -f "${KEY_FILE}" ]]; then
-    # POSTFIX: TLS in /etc/postfix/main.cf
-    postconf -e smtpd_tls_cert_file=${CRT_FILE}
-    postconf -e smtpd_tls_key_file=${KEY_FILE}
-    postconf -e smtpd_tls_security_level=may
-    postconf -e smtp_tls_security_level=may
-    # POSTFIX: TLS in /etc/postfix/master.cf
-    postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-    postconf -P "submission/inet/syslog_name=postfix/submission"
-    postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
-else 
-    echo 'CRITICAL ERROR: Failed to find TLS cert files'
-    exit
+if ![[ -f "${CRT_FILE}" && -f "${KEY_FILE}" ]]; then
+    echo "CRITICAL ERROR: Failed to find TLS cert file ${KEY_FILE} or key file ${KEY_FILE}"
 fi
+
+# POSTFIX: TLS in /etc/postfix/main.cf
+postconf -e smtpd_tls_cert_file=${CRT_FILE}
+postconf -e smtpd_tls_key_file=${KEY_FILE}
+postconf -e smtpd_tls_security_level=may
+postconf -e smtp_tls_security_level=may
+# POSTFIX: TLS in /etc/postfix/master.cf
+postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
+postconf -P "submission/inet/syslog_name=postfix/submission"
+postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
 
