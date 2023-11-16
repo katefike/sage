d0665afbeec6b5b4166c0a9ef6b07f118123106b
Kate Fike <kfike@pop-os.localdomain>
2023-11-16 15:09:31 +0700

    Temporarily remove TLS requirements and open port 25

--- docker/mailserver/scripts/postfix_dovecot_config_prod.sh
+++ docker/mailserver/scripts/postfix_dovecot_config_prod.sh
@@ -9,13 +9,13 @@ if ![[ -f "${CRT_FILE}" && -f "${KEY_FILE}" ]]; then
     echo "CRITICAL ERROR: Failed to find TLS cert file ${KEY_FILE} or key file ${KEY_FILE}"
 fi
 
-# POSTFIX: TLS in /etc/postfix/main.cf
-postconf -e smtpd_tls_cert_file=${CRT_FILE}
-postconf -e smtpd_tls_key_file=${KEY_FILE}
-postconf -e smtpd_tls_security_level=may
-postconf -e smtp_tls_security_level=may
-# POSTFIX: TLS in /etc/postfix/master.cf
-postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-postconf -P "submission/inet/syslog_name=postfix/submission"
-postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
+# # POSTFIX: TLS in /etc/postfix/main.cf
+# postconf -e smtpd_tls_cert_file=${CRT_FILE}
+# postconf -e smtpd_tls_key_file=${KEY_FILE}
+# postconf -e smtpd_tls_security_level=may
+# postconf -e smtp_tls_security_level=may
+# # POSTFIX: TLS in /etc/postfix/master.cf
+# postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
+# postconf -P "submission/inet/syslog_name=postfix/submission"
+# postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
 
