b10c32f5050d0ed8e4c9d991813775f92bf09352
Kate Fike <kfike@pop-os.localdomain>
2024-02-27 23:53:23 +0700

    Remove --dry-run

--- docker/certbot/get_certs.sh
+++ docker/certbot/get_certs.sh
@@ -59,7 +59,7 @@ else
         -p 80:80 \
         -v "/etc/letsencrypt:/etc/letsencrypt" \
         -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-        certbot/certbot renew --dry-run
+        certbot/certbot renew
         # TODO: Use --deploy-hook to load certs and restart mailserver container
     fi
 
