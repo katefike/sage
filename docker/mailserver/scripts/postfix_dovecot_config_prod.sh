ba8b91de0104c0979c6b04c8709c419a1b6f7683
Kate Fike <kfike@pop-os.localdomain>
2024-02-28 07:29:22 +0700

    Uncomment config for required encryption

--- docker/mailserver/scripts/postfix_dovecot_config_prod.sh
+++ docker/mailserver/scripts/postfix_dovecot_config_prod.sh
@@ -12,11 +12,11 @@ fi
 # POSTFIX: TLS in /etc/postfix/main.cf
 postconf -e smtpd_tls_cert_file=${CRT_FILE}
 postconf -e smtpd_tls_key_file=${KEY_FILE}
-postconf -e smtpd_tls_security_level=may
-# # POSTFIX: TLS in /etc/postfix/master.cf
-# postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-# postconf -P "submission/inet/syslog_name=postfix/submission"
-# postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
+postconf -e smtpd_tls_security_level=encrypt
+# POSTFIX: TLS in /etc/postfix/master.cf
+postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
+postconf -P "submission/inet/syslog_name=postfix/submission"
+postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
 
 # Configures /etc/dovecot/dovecot.conf for production
 # Clear the file contents
