1428895fbf3e36a2942116e17d24476eb0625ee6
kfike <kate.e.fike@gmail.com>
2024-05-22 19:18:01 -0400

    Add logs for attempting to parse, failed to parse, and successful parse

--- sage/__main__.py
+++ sage/__main__.py
@@ -73,13 +73,15 @@ def main():
 
             # Parse a email message into the transaction data
             transaction = email_parser.main(msg, email_id)
+            logger.info(f"Email UID {msg.uid} - attempting to parse...")
             if not transaction:
-                logger.info(f"UID {msg.uid} was not parsed into a transaction.")
+                logger.info(f"Email UID {msg.uid} - unparsed.")
                 msg_count["unparsed"] = msg_count.get("unparsed", 0) + 1
                 continue
 
             # Write the transaction to the database
             transactions.insert_transaction(transaction)  # pragma: no cover
+            logger.info(f"Email UID {msg.uid} - successfully parsed!")
 
             # One down!
             msg_count["processed"] = (
