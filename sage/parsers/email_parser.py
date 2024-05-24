685f8e45e257ee159c519476afa799e2fee56452
kfike <kate.e.fike@gmail.com>
2024-05-24 00:15:47 -0400

    Fix f string

--- sage/parsers/email_parser.py
+++ sage/parsers/email_parser.py
@@ -90,7 +90,7 @@ def get_date(body: str) -> str:
         transformed_date = datetime.strftime(datetime_raw_date, "%Y-%m-%d")
         return transformed_date
     else:
-        raise RegexError("Regex failed to get the date from body: {body}")
+        raise RegexError(f"Regex failed to get the date from body: {body}")
 
 
 def get_bank(body: str) -> str:
@@ -121,12 +121,12 @@ def parse_chase(subject: MailMessage.subject) -> str:
     merchant = regex_search(r"(?<=with )(.*)", subject)
     if merchant is None:
         raise RegexError(
-            "Regex failed to get the merchant from a Chase email subject: {subject}"
+            f"Regex failed to get the merchant from a Chase email subject: {subject}"
         )
     raw_amount = regex_search(r"(?<=\$)(.*)(?= transaction)", subject)
     if raw_amount is None:
         raise RegexError(
-            "Regex failed to get the raw amount from a Chase email subject: {subject}"
+            f"Regex failed to get the raw amount from a Chase email subject: {subject}"
         )
     return merchant, raw_amount
 
@@ -142,7 +142,15 @@ def parse_discover(body: str) -> str:
     Amount: $23.50
     """
     merchant = regex_search(r"(?<=Merchant: )(.*)(?= Amount: )", body)
+    if merchant is None:
+        raise RegexError(
+            f"Regex failed to get the merchant from a Discover email body: {body}"
+        )
     raw_amount = regex_search(r"(?<=Amount: \$)([0-9]+(?:,[0-9]{3})?\.[0-9]{2})", body)
+    if raw_amount is None:
+        raise RegexError(
+            f"Regex failed to get the raw amount from a Discover email body: {body}"
+        )
     return merchant, raw_amount
 
 
