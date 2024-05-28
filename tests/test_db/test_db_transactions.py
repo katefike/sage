2653ce2d0e5792f4e7cf8c524fb410ec18c0cedb
kfike <kate.e.fike@gmail.com>
2024-05-27 22:55:23 -0400

    Add None for transaction obj id

--- tests/test_db/test_db_transactions.py
+++ tests/test_db/test_db_transactions.py
@@ -10,13 +10,16 @@ viewed.
 The expected expected_output is the transaction object defined in
 sage/models/transaction.py
 """
+from typing import List
+
 import pytest
 
 from sage.db import transactions
+from sage.models.email import Email
 from sage.models.transaction import Transaction
 from tests import utils
 
-data = [
+transaction_data = [
     (
         dict(
             date="2022-10-06",
@@ -128,9 +131,9 @@ data = [
 ]
 
 
-def create_transaction_objects():
+def create_transaction_objects() -> List:
     input = []
-    for transaction_dict in data:
+    for transaction_dict in transaction_data:
         transaction = Transaction(
             0,
             transaction_dict.get("date"),
@@ -156,3 +159,49 @@ def test_insert_transaction(input):
     row_count = transactions.insert_transaction(input)
     # The number of rows inserted will be returned if the insert was successful
     assert 1 == row_count
+
+
+email_data = [
+    dict(
+        uid=1,
+        batch_time="2023-08-31 15:22:40",
+        forwarded_date="2023-08-31",
+        from_="outgoing@gmail.com",
+        origin="bank@example.com",
+        subject="Example Transaction Email",
+        html="f",
+        body="Hello world!",
+    )
+]
+
+
+def create_email_objects() -> List:
+    input = []
+    for email_dict in email_data:
+        email = Email(
+            0,
+            None,
+            email_dict.get("uid"),
+            email_dict.get("batch_time"),
+            email_dict.get("forwarded_date"),
+            email_dict.get("from_"),
+            email_dict.get("origin"),
+            email_dict.get("subject"),
+            email_dict.get("html"),
+            email_dict.get("body"),
+        )
+        input.append(email)
+    return input
+
+
+# @pytest.mark.parametrize("input", create_email_objects())
+# def test_duplicate_email_forwards(input):
+#     """
+#     Ensure that if the same transaction email is forwarded twice,
+#     the result is only one transaction.
+#     """
+#     email_id = utils.insert_db_email(input)
+#     input.email_id = email_id
+#     row_count = transactions.insert_transaction(input)
+#     # The number of rows inserted will be returned if the insert was successful
+#     assert 1 == row_count
