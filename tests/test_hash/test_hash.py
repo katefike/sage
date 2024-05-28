15daad4972ab3763400aacda807f8b86f4d0731c
kfike <kate.e.fike@gmail.com>
2024-05-28 10:27:07 -0400

    Add docstring

--- tests/test_hash/test_hash.py
+++ tests/test_hash/test_hash.py
@@ -1,62 +1,56 @@
 """
-Tests inserting transactions (module sage/db/transactions.py).
+Tests hashing, which is a way to screen for duplicate transactions.
 
-The input is a transaction class object.
+The input is a set of two or more emails. The emails may or may not represent
+duplicate transactions.
 
-They're also listed as separate files in
-tests/test_parsers/test_data/example_data so they can be more easily
-viewed.
-
-The expected expected_output is the transaction object defined in
-sage/models/transaction.py
+The expected output is the failure or success of inserting a transaction.
+For two similar, but distinct transactions, the output is two inserted
+transactions.
+For duplicate transactions (e.g. from the same transaction forwarded twice)
+the output is one inserted transaction.
 """
 import pytest
 
 from sage.db import transactions
-from sage.models.transaction import Transaction
+from sage.models.email import Email
 from tests import utils
 
-email_data = [
-    dict(
-        uid=1,
-        batch_time="2023-08-31 15:22:40",
-        forwarded_date="2023-08-31",
-        from_="outgoing@gmail.com",
-        origin="bank@example.com",
-        subject="Example Transaction Email",
-        html="f",
-        body="Hello world!",
-    )
-]
+
+def get_test_data():
+    data = [
+        # (dict(file="distinct_but_similar_txns.mbox"), dict(insert_count=2)),
+        (dict(file="duplicate_txns_gmail+cloudHQ_forwards.mbox"), dict(insert_count=1)),
+        (dict(file="duplicate_txns_identical_forwards.mbox"), dict(insert_count=1)),
+    ]
+
+    # Retrieve the email corresponding to the UID
+    for email in data:
+        input = email[0]
+        uid = input.get("uid")
+        msgs = utils.get_inbox_emails(uid)
+        if len(msgs) == 0:
+            print(f"CRITICAL: No email having UID {uid} was found.")
+        if len(msgs) > 1:
+            print(f"CRITICAL: More than one email having UID {uid} was found.")
+        # Iterate over messages,
+        # but we're only expecting a single email message in the object.
+        for msg in msgs:
+            input["msg"] = msg
+
+        input["email_id"] = input.get("email_id")
+
+    return data
 
 
-def create_email_objects() -> List:
-    input = []
-    for email_dict in email_data:
-        email = Email(
-            0,
-            None,
-            email_dict.get("uid"),
-            email_dict.get("batch_time"),
-            email_dict.get("forwarded_date"),
-            email_dict.get("from_"),
-            email_dict.get("origin"),
-            email_dict.get("subject"),
-            email_dict.get("html"),
-            email_dict.get("body"),
-        )
-        input.append(email)
-    return input
+utils.fresh_inbox("transaction_emails.mbox")
+DATA = get_test_data()
 
 
-@pytest.mark.parametrize("input", create_email_objects())
-def test_duplicate_email_forwards(input):
+@pytest.mark.parametrize("input,expected_output", DATA)
+def test_hash(input, expected_output):
     """
-    Ensure that if the same transaction email is forwarded twice,
-    the result is only one transaction.
+    Ensure that duplicate transactions are rejected based on the hash value.
     """
-    email_id = utils.insert_db_email(input)
-    input.email_id = email_id
-    row_count = transactions.insert_transaction(input)
-    # The number of rows inserted will be returned if the insert was successful
-    assert 1 == row_count
+    # transaction = email_parser.main(input.get("msg"), input.get("email_id"))
+    # assert expected_output.get("bank") == transaction.bank
