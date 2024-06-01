2369d6ea5398e2b74b5b09766c7a69781d3e4f3d
kfike <kate.e.fike@gmail.com>
2024-05-31 21:28:01 -0400

    Move ENV initialization to separate module

--- sage/__init__.py
+++ sage/__init__.py
@@ -1,17 +1,1 @@
-import os
-import pathlib
-
-from dotenv import dotenv_values
-
-if os.environ.get("ISDEV") == "True":
-    ISDEV = True
-    ENV_FILE = "/.env-example"
-else:
-    ISDEV = False
-    ENV_FILE = "/.env"
-
-APP_ROOT = str(pathlib.Path(__file__).parent.parent)
-
-ENV = dotenv_values(APP_ROOT + ENV_FILE)
-
-ENV["ISDEV"] = ISDEV
+from sage.config import ENV
