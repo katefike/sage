ffb9de23a3a6202d86cf3e3e43c0de4c6d2d6e18
kfike <kate.e.fike@gmail.com>
2024-06-04 10:13:23 -0400

    Load env from sage.config

--- scripts/get_all_emails.py
+++ scripts/get_all_emails.py
@@ -1,8 +1,4 @@
-import os
-import pathlib
-
 import imap_tools
-from dotenv import load_dotenv
 
 from sage.config import ENV
 
@@ -13,17 +9,13 @@ It's hard to look at the email body from the command line,
 it's easier to redirect stdout to a file by adding this like to the command.
 > scripts/debug_stdout.txt
 """
-# Get all environment variables
-app_root = str(pathlib.Path(__file__).parent.parent)
-env_path = app_root + "/.env"
-if not load_dotenv(env_path):
-    print(f"ENVIRONMENT ERROR: .env failed to load from {env_path}")
-FORWARDING_EMAIL = os.environ.get("FORWARDING_EMAIL")
-RECEIVING_EMAIL_USER = os.environ.get("RECEIVING_EMAIL_USER")
-RECEIVING_EMAIL_PASSWORD = os.environ.get("RECEIVING_EMAIL_PASSWORD")
+FORWARDING_EMAIL = ENV["FORWARDING_EMAIL"]
+RECEIVING_EMAIL_USER = ENV["RECEIVING_EMAIL_USER"]
+RECEIVING_EMAIL_PASSWORD = ENV["RECEIVING_EMAIL_PASSWORD"]
 
 count = 0
 try:
+    print(f"Connecting to mailbox with ")
     with imap_tools.MailBoxUnencrypted("localhost").login(
         RECEIVING_EMAIL_USER, RECEIVING_EMAIL_PASSWORD
     ) as mailbox:
