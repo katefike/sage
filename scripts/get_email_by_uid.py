acc77ae6b027d0a15b1e1dd572a39508871ae06d
kfike <kate.e.fike@gmail.com>
2024-06-04 10:25:56 -0400

    Remove unused FORWARDING_EMAIL

--- scripts/get_email_by_uid.py
+++ scripts/get_email_by_uid.py
@@ -14,8 +14,6 @@ It's hard to look at the email body from the command line,
 it's easier to redirect stdout to a file by adding this like to the command.
 > scripts/debug_stdout.txt
 """
-# Get all environment variables
-FORWARDING_EMAIL = ENV["FORWARDING_EMAIL"]
 RECEIVING_EMAIL_USER = ENV["RECEIVING_EMAIL_USER"]
 RECEIVING_EMAIL_PASSWORD = ENV["RECEIVING_EMAIL_PASSWORD"]
 
@@ -24,6 +22,7 @@ input_uid = sys.argv[1]
 
 count = 0
 try:
+    print(f"Connecting to mailbox of user {RECEIVING_EMAIL_USER}...")
     with imap_tools.MailBoxUnencrypted("localhost").login(
         RECEIVING_EMAIL_USER, RECEIVING_EMAIL_PASSWORD
     ) as mailbox:
@@ -39,7 +38,7 @@ try:
                 print(f"HTML: {msg.html}")
         print(f"{count} emails were retrieved.")
 except imap_tools.MailboxLoginError as error:
-    print("FAILED")
     print(
-        f"MAILSERVER ERROR: Failed to connect via IMAP to the inbox of user {RECEIVING_EMAIL_USER}: {error}"
+        f"MAILSERVER ERROR: Failed to connect via IMAP to the inbox of user \
+            {RECEIVING_EMAIL_USER}: {error}"
     )
