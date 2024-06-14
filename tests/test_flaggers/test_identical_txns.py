99c96d10397cc0789a8ddb2d38de8b2a519f53c0
kfike <kate.e.fike@gmail.com>
2024-06-14 10:11:36 -0400

    Change test paradigm to insert possible identical txns and flag

---
+++ tests/test_flaggers/test_identical_txns.py
@@ -0,0 +1,46 @@
+"""
+Tests hashing, which is a way to screen for duplicate transactions.
+
+The input is a set of two or more emails. The emails may or may not represent
+duplicate transactions.
+
+The expected output is the failure or success of inserting a transaction.
+For two similar, but distinct transactions, the output is two inserted
+transactions.
+For duplicate transactions (e.g. from the same transaction forwarded twice)
+the output is one inserted transaction.
+"""
+import os
+
+import pytest
+
+from sage.db import transactions
+from tests import utils
+
+DATA = [
+    # (dict(file="distinct_but_similar_txns.mbox"), dict(inserted_txn_count=2, flagged_identical_txn=1)),
+    (
+        dict(file="identical_txns_gmail+cloudHQ_forwards.mbox"),
+        dict(inserted_txn_count=2, flagged_identical_txn=1),
+    ),
+    (
+        dict(file="identical_txns_duplicate_forwards.mbox"),
+        dict(inserted_txn_count=2, flagged_identical_txn=1),
+    ),
+    # (
+    #     dict(file="identical_txns_triplicate_forwards.mbox"),
+    #     dict(inserted_txn_count=3, flagged_identical_txn=2),
+    # ),
+]
+
+
+@pytest.mark.parametrize("input,expected_output", DATA)
+def test_identical_txns(input, expected_output):
+    """
+    Ensure that potential identical transactions are flagged.
+    """
+    utils.refresh_inbox(input.get("file"))
+    from sage.__main__ import main
+
+    msg_count = main()
+    assert expected_output.get("inserted_txn_count") == msg_count.get("processed")
