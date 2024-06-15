14fb208f688a198f7e4d29bb52fe0251426d9f7f
kfike <kate.e.fike@gmail.com>
2024-06-15 11:43:25 -0400

    Add FIXME

--- sage/models/transaction.py
+++ sage/models/transaction.py
@@ -38,6 +38,7 @@ class Transaction:
     # there is only one account associated with the bank.
     account: str = field(default=None)
     # Not all transactions list the account balance.
+    # FIXME: Add balance to txn table #156
     balance: str = field(default=None)
     # The ID of an older txn that's identical to this one
     identical_txn_id: id = field(default=None)
