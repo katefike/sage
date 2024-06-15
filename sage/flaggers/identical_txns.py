9f7945c04bd72c84418f920c6f1110b521495a94
kfike <kate.e.fike@gmail.com>
2024-06-15 13:24:14 -0400

    Call function to get identical txn ID

--- sage/flaggers/identical_txns.py
+++ sage/flaggers/identical_txns.py
@@ -1,6 +1,17 @@
+"""
+Identify the oldest txn that has the same attributes as the current txn.
+"""
+
+from loguru import logger
+
 from sage.db import transactions
 from sage.models.transaction import Transaction
 
+logger.add(sink="sage_main.log")
 
-def main(txn: Transaction):
+
+def main(txn: Transaction) -> Transaction:
+    txn.identical_txn_id = transactions.get_identical_txn_id(txn)
+    if txn.identical_txn_id is not None:
+        logger.warning(f"Idential txn ID {txn.identical_txn_id} exists for txn {txn}.")
     return txn
