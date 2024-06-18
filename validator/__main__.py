b152836eeaf7796b55344c85ef123640d61b6906
kfike <kate.e.fike@gmail.com>
2024-06-18 09:56:40 -0400

    Fix function name

--- validator/__main__.py
+++ validator/__main__.py
@@ -134,7 +134,7 @@ def get_csv_data(file: str, dates: List) -> List:
 
 
 def get_db_data(start_date: str, stop_date: str, bank: str, account: str) -> List:
-    db_records = transactions.get_txns_by_daterange__and_bank_account(
+    db_records = transactions.get_txns_by_daterange_and_bank_account(
         start_date, stop_date, bank, account
     )
     db_data = []
