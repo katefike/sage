b5c4b005d8f8881d18b5f072107fca8edfe47644
kfike <kate.e.fike@gmail.com>
2024-06-19 20:25:12 -0400

    Create main function for getting emails

--- sage/mx/get_emails.py
+++ sage/mx/get_emails.py
@@ -4,18 +4,14 @@ from typing import List, Optional
 import imap_tools
 from loguru import logger
 
+from sage.models.email import Email
+
 from . import ENV
 
 logger.add(sink="sage_main.log")
 
-logger.info(f"FORWARDING_EMAIL: {ENV['FORWARDING_EMAIL']}")
 
-# Set the time the batch started
-utc_timestamp = datetime.utcnow()
-batch_time = utc_timestamp.strftime("%Y-%m-%d %H:%M:%S")
-
-
-def open_mailbox():
+def open_mailbox() -> imap_tools.BaseMailBox:
     try:
         mailbox = imap_tools.MailBoxUnencrypted("localhost").login(
             ENV["RECEIVING_EMAIL_USER"], ENV["RECEIVING_EMAIL_PASSWORD"]
@@ -26,6 +22,58 @@ def open_mailbox():
         raise error
 
 
-def get_emails():
+def main(
+    from_forwarding_email: Optional[bool],
+    retry_unparsed_emails: Optional[bool],
+    uid_: Optional[int],
+) -> List[Email]:
     mailbox = open_mailbox()
-    return
+
+    if from_forwarding_email:
+        logger.info(
+            f"Only getting emails from FORWARDING_EMAIL {ENV['FORWARDING_EMAIL']}..."
+        )
+        msgs = mailbox.fetch(imap_tools.A(from_=ENV["FORWARDING_EMAIL"]))
+    elif retry_unparsed_emails:
+        logger.info(
+            "Only getting emails that are in the DB table named emails, \
+            but don't have an associated txn..."
+        )
+    elif uid_:
+        logger.info(f"Only getting email uid {uid_}...")
+        msgs = mailbox.fetch(imap_tools.AND(uid=[uid_]))
+    else:
+        logger.info("Getting all emails from inbox...")
+        msgs = mailbox.fetch()
+
+    # Ultimately returned messages
+    emails = []
+
+    # Set the time the batch started
+    utc_timestamp = datetime.utcnow()
+    batch_time = utc_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+    # FIXME: type msg as class imap_tools.MailMessage
+    for msg in msgs:
+        # FIXME: Add origin to the emails table #157
+        origin = "placeholder"
+        # FIXME: body is set twice: once below and once in email_parser
+        if msg.html:
+            html = "true"
+            body = msg.html
+        elif msg.text:
+            html = "false"
+            body = msg.text
+        email = Email(
+            int(msg.uid),
+            batch_time,
+            msg.date,
+            msg.from_,
+            origin,
+            msg.subject,
+            html,
+            body,
+        )
+        emails.append(email)
+
+    return emails
