4805fc84776dc41c1b6cb49791ff7a6b9ee0ad46
kfike <kate.e.fike@gmail.com>
2024-06-20 10:59:10 -0400

    Use mailbox in With block
    Fixes error  Failed to open mailbox: Response status OK expected, but NO received. Data: [b'[UNAVAILABLE] Maximum number of connections from user+IP exceeded (mail_max_userip_connections=10)']

--- sage/mx/get_emails.py
+++ sage/mx/get_emails.py
@@ -18,47 +18,46 @@ def main(
     filter: Optional[str] = None,
     pls_print: Optional[bool] = False,
 ) -> List[Email]:
-    mailbox = open_mailbox()
+    with open_mailbox() as mailbox:
+        if filter == "forwarded":
+            logger.info(
+                f"Only getting emails from FORWARDING_EMAIL {ENV['FORWARDING_EMAIL']}..."
+            )
+            msgs = mailbox.fetch(imap_tools.A(from_=ENV["FORWARDING_EMAIL"]))
 
-    if filter == "forwarded":
-        logger.info(
-            f"Only getting emails from FORWARDING_EMAIL {ENV['FORWARDING_EMAIL']}..."
-        )
-        msgs = mailbox.fetch(imap_tools.A(from_=ENV["FORWARDING_EMAIL"]))
+        elif filter == "unparsed":
+            logger.info(
+                "Only getting emails that are in the DB table named emails,\
+                but don't have an associated txn..."
+            )
+            msgs = []
+            records, columns_ = emails.get_unparsed_emails()
+            for record in records:
+                uid_ = record[1]
+                retrieved_msg = mailbox.fetch(imap_tools.AND(uid=[str(uid_)]))
+                for msg in retrieved_msg:
+                    msgs.append(msg)
 
-    elif filter == "unparsed":
-        logger.info(
-            "Only getting emails that are in the DB table named emails,\
-            but don't have an associated txn..."
-        )
-        msgs = []
-        records, columns_ = emails.get_unparsed_emails()
-        for record in records:
-            uid_ = record[1]
-            retrieved_msg = mailbox.fetch(imap_tools.AND(uid=[str(uid_)]))
-            for msg in retrieved_msg:
-                msgs.append(msg)
+        elif filter and "uid=" in filter:
+            uid_parts = filter.split("=")
+            uid_ = uid_parts[1]
+            logger.info(f"Only getting email uid {uid_}...")
+            msgs = mailbox.fetch(imap_tools.AND(uid=[uid_]))
+            if msgs is None:
+                logger.critical(f"No email was retrieved for email UID {uid_}...")
+                return
+        else:
+            logger.info("Getting all emails from inbox...")
+            msgs = mailbox.fetch()
 
-    elif filter and "uid=" in filter:
-        uid_parts = filter.split("=")
-        uid_ = uid_parts[1]
-        logger.info(f"Only getting email uid {uid_}...")
-        msgs = mailbox.fetch(imap_tools.AND(uid=[uid_]))
-        if msgs is None:
-            logger.critical(f"No email was retrieved for email UID {uid_}...")
-            return
-    else:
-        logger.info("Getting all emails from inbox...")
-        msgs = mailbox.fetch()
+        emails_ = transform_MailMessages_to_Emails(msgs)
 
-    emails_ = transform_MailMessages_to_Emails(msgs)
+        if pls_print:
+            pprint.pp(emails_)
 
-    if pls_print:
-        pprint.pp(emails_)
+        logger.info(f"{len(emails_)} email(s) retrieved.")
 
-    logger.info(f"{len(emails_)} email(s) retrieved.")
-
-    return emails_
+        return emails_
 
 
 def open_mailbox() -> imap_tools.BaseMailBox:
