6700620e9500ff7d88e49479cac98731ba387e6d
kfike <kate.e.fike@gmail.com>
2024-06-25 21:13:36 -0400

    Move diff function to bank modules

--- validator/__main__.py
+++ validator/__main__.py
@@ -4,16 +4,15 @@ a bank's CSV of transactions. This module isn't use by the Sage program,
 it's used to facilitate local development and troubleshooting.
 """
 import calendar
-import copy
 import sys
 from datetime import datetime
-from typing import Dict, List
+from typing import List, Optional
 
 from loguru import logger
 
 from sage.db import transactions
 from sage.models.transaction import Transaction
-from validator import get_csv_data
+from validator.banks import Chase, Huntington
 
 logger.add(sink="validator.log", level="INFO")
 
@@ -47,14 +46,16 @@ def main(file: str, date: str):
             f"Getting DB transaction data from {start_date} to {stop_date}, bank {bank}, account {account}."
         )
 
-    db_data = get_db_data(start_date, stop_date, bank, account)
-
-    # Get CSV data
-    logger.info(f"Getting data from validation CSV file: {file}")
-    csv_data = get_csv_data.Huntington.main(file, dates)
-
-    # Diff the CSV and DB data, row-to-record
-    diff = diff_csv_and_db_data(csv_data, db_data)
+    if bank == "Huntington":
+        db_data = get_db_data(start_date, stop_date, bank, account)
+        logger.info(f"Getting data from validation CSV file: {file}")
+        csv_data = Huntington.get_csv_data(file, dates)
+        diff = Huntington.diff_csv_and_db_data(csv_data, db_data)
+    if bank == "Chase":
+        db_data = get_db_data(start_date, stop_date, bank, account)
+        logger.info(f"Getting data from validation CSV file: {file}")
+        csv_data = Chase.get_csv_data(file, dates)
+        diff = Chase.diff_csv_and_db_data(csv_data, db_data)
 
     logger.info("CSV rows not in DB:")
     for i, csv_row in enumerate(diff.get("CSV rows not in DB")):
@@ -121,7 +122,9 @@ def transform_zero_padded_dates(raw_dates: List) -> List:
     return dates
 
 
-def get_db_data(start_date: str, stop_date: str, bank: str, account: str) -> List:
+def get_db_data(
+    start_date: str, stop_date: str, bank: str, account: Optional[str]
+) -> List:
     db_records = transactions.get_txns_by_daterange_and_bank_account(
         start_date, stop_date, bank, account
     )
@@ -150,96 +153,6 @@ def get_db_data(start_date: str, stop_date: str, bank: str, account: str) -> Lis
         txn.type_ = db_record[7]
         db_data.append(txn)
     return db_data
-
-
-def diff_csv_and_db_data(csv_data: List, db_data: List) -> Dict:
-    """
-    csv_data:
-        [{
-            "Date": "04/01/2024",
-            "Payee Name": "UBER BV          IAT PAYPAL",
-            "Amount": "-0.51",
-        },
-        {
-            "Date": "04/02/2024",
-            "Payee Name": "INTERNET TFR FRM SAVINGS",
-            "Amount": "1000.01",
-        },]
-
-    db_data:
-        [Transaction(
-            date="04/01/2024",
-            merchant="UBER BV IAT PAYPAL",
-            amount=Decimal("-0.51"),
-        ),
-        Transaction(
-            date="04/03/2024",
-            merchant="DISCOVER         E-PAYMENT",
-            amount=Decimal("-49.53"),
-        ),]
-    diff:
-        {
-            "CSV rows not in DB": {
-                "Date": "04/02/2024",
-                "Payee Name": "INTERNET TFR FRM SAVINGS",
-                "Amount": "1000.01",
-            },
-            "DB records not in CSV": Transaction(
-                date="04/03/2024",
-                merchant="DISCOVER         E-PAYMENT",
-                amount=Decimal("-49.53"),
-            ),
-        }
-    """
-    # Compare total CSV rows to total DB records
-    total_csv_rows = len(csv_data)
-    logger.info(f"Total CSV rows: {total_csv_rows}")
-    total_db_records = len(db_data)
-    logger.info(f"Total DB records: {total_db_records}")
-    if total_csv_rows != total_db_records:
-        logger.error("Total rows in CSV differs from DB!")
-
-    diff = {}
-
-    # Identify CSV rows not in DB
-    csv_data_copy = copy.deepcopy(csv_data)
-    for Transaction in db_data:
-        for csv_row in csv_data_copy:
-            if Transaction.date != csv_row.get("Date"):
-                continue
-            # Convert Transaction decimal to string
-            if str(Transaction.amount) != csv_row.get("Amount"):
-                continue
-            if "transfer" not in Transaction.type_:
-                # The Huntington CSV sometimes includes a lot of extra spaces
-                if Transaction.merchant.replace(" ", "") != csv_row.get(
-                    "Payee Name"
-                ).replace(" ", ""):
-                    continue
-            # Remove row from CSV data if all three fields are matched
-            csv_data_copy.remove(csv_row)
-    diff["CSV rows not in DB"] = csv_data_copy
-
-    # Identify DB records not in CSV
-    db_data_copy = copy.deepcopy(db_data)
-    for csv_row in csv_data:
-        for Transaction in db_data_copy:
-            if Transaction.date != csv_row.get("Date"):
-                continue
-            # Convert Transaction decimal to string
-            if str(Transaction.amount) != csv_row.get("Amount"):
-                continue
-            if "transfer" not in Transaction.type_:
-                # The Huntington CSV sometimes includes a lot of extra spaces
-                if Transaction.merchant.replace(" ", "") != csv_row.get(
-                    "Payee Name"
-                ).replace(" ", ""):
-                    continue
-            # Remove row from CSV data if all three fields are matched
-            db_data_copy.remove(Transaction)
-    diff["DB records not in CSV"] = db_data_copy
-
-    return diff
 
 
 if __name__ == "__main__":  # pragma: no cover
