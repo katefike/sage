b62ec55aeab4ba2f36913d83e0507dc6705145b8
kfike <kate.e.fike@gmail.com>
2024-06-25 21:59:57 -0400

    Git rid of banks modules again

--- validator/__main__.py
+++ validator/__main__.py
@@ -4,17 +4,22 @@ a bank's CSV of transactions. This module isn't use by the Sage program,
 it's used to facilitate local development and troubleshooting.
 """
 import calendar
+import copy
+import csv
+import pathlib
 import sys
 from datetime import datetime
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from loguru import logger
 
 from sage.db import transactions
 from sage.models.transaction import Transaction
-from validator.banks import Chase, Huntington
 
 logger.add(sink="validator.log", level="INFO")
+
+APP_ROOT = str(pathlib.Path(__file__).parent.parent)
+FILE_PATH = APP_ROOT + "/validator/real_data/"
 
 
 def main(file: str, date: str):
@@ -37,6 +42,16 @@ def main(file: str, date: str):
     bank = file_parts[0]
     account = file_parts[1]
 
+    if bank == "Huntington":
+        csv_col_mapping = dict(
+            date_col="Date", merchant_col="Payee Name", amount_col="Amount"
+        )
+    if bank == "Chase":
+        account = None
+        csv_col_mapping = dict(
+            date_col="Transaction Date", merchant_col="Description", amount_col="Amount"
+        )
+
     if start_date == stop_date:
         logger.info(
             f"Getting DB transaction data for {start_date}, bank {bank}, account {account}."
@@ -46,22 +61,17 @@ def main(file: str, date: str):
             f"Getting DB transaction data from {start_date} to {stop_date}, bank {bank}, account {account}."
         )
 
-    if bank == "Huntington":
-        db_data = get_db_data(start_date, stop_date, bank, account)
-        logger.info(f"Getting data from validation CSV file: {file}")
-        csv_data = Huntington.get_csv_data(file, dates)
-        diff = Huntington.diff_csv_and_db_data(csv_data, db_data)
-    if bank == "Chase":
-        account = None
-        db_data = get_db_data(start_date, stop_date, bank, account)
-        logger.info(f"Getting data from validation CSV file: {file}")
-        csv_data = Chase.get_csv_data(file, dates)
-        diff = Chase.diff_csv_and_db_data(csv_data, db_data)
+    db_data = get_db_data(start_date, stop_date, bank, account)
+
+    logger.info(f"Getting data from validation CSV file: {file}")
+    csv_data = get_csv_data(file, dates, csv_col_mapping)
+
+    diff = diff_csv_and_db_data(csv_data, db_data, csv_col_mapping)
 
     logger.info("CSV rows not in DB:")
     for i, csv_row in enumerate(diff.get("CSV rows not in DB")):
         logger.info(
-            f"{i + 1} - {csv_row['Date']}, {csv_row['Payee Name']}, {csv_row['Amount']}"
+            f"{i + 1} - {csv_row[csv_col_mapping.get('date_col')]}, {csv_row[csv_col_mapping.get('merchant_col')]}, {csv_row[csv_col_mapping.get('amount_col')]}"
         )
 
     logger.info("DB records not in CSV:")
@@ -154,6 +164,112 @@ def get_db_data(
         txn.type_ = db_record[7]
         db_data.append(txn)
     return db_data
+
+
+def get_csv_data(file: str, dates: List, csv_col_mapping: Dict) -> List:
+    with open(FILE_PATH + file, mode="r", encoding="utf-8") as open_csv:
+        reader = csv.DictReader(open_csv)
+        csv_data = []
+
+        for row in reader:
+            row_date = row[csv_col_mapping.get("date_col")]
+            if row_date in dates:
+                csv_data.append(row)
+    return csv_data
+
+
+def diff_csv_and_db_data(csv_data: List, db_data: List, csv_col_mapping: Dict) -> Dict:
+    """
+    Huntington csv_data :
+        [{
+            "Date": "04/01/2024",
+            "Payee Name": "UBER BV          IAT PAYPAL",
+            "Amount": "-0.51",
+        },
+        {
+            "Date": "04/02/2024",
+            "Payee Name": "INTERNET TFR FRM SAVINGS",
+            "Amount": "1000.01",
+        },]
+
+    db_data:
+        [Transaction(
+            date="04/01/2024",
+            merchant="UBER BV IAT PAYPAL",
+            amount=Decimal("-0.51"),
+        ),
+        Transaction(
+            date="04/03/2024",
+            merchant="DISCOVER         E-PAYMENT",
+            amount=Decimal("-49.53"),
+        ),]
+    diff:
+        {
+            "CSV rows not in DB": {
+                "Date": "04/02/2024",
+                "Payee Name": "INTERNET TFR FRM SAVINGS",
+                "Amount": "1000.01",
+            },
+            "DB records not in CSV": Transaction(
+                date="04/03/2024",
+                merchant="DISCOVER         E-PAYMENT",
+                amount=Decimal("-49.53"),
+            ),
+        }
+    """
+    # Compare total CSV rows to total DB records
+    total_csv_rows = len(csv_data)
+    logger.info(f"Total CSV rows: {total_csv_rows}")
+    total_db_records = len(db_data)
+    logger.info(f"Total DB records: {total_db_records}")
+    if total_csv_rows != total_db_records:
+        logger.error("Total rows in CSV differs from DB!")
+
+    diff = {}
+
+    # Identify CSV rows not in DB
+    csv_data_copy = copy.deepcopy(csv_data)
+    for Transaction in db_data:
+        for csv_row in csv_data_copy:
+            if Transaction.date != csv_row.get(csv_col_mapping.get("date_col")):
+                continue
+            # Convert Transaction decimal to string
+            if str(Transaction.amount) != csv_row.get(
+                csv_col_mapping.get("amount_col")
+            ):
+                continue
+            if "transfer" not in Transaction.type_:
+                # The Huntington CSV sometimes includes a lot of extra spaces
+                if Transaction.merchant.replace(" ", "") != csv_row.get(
+                    csv_col_mapping.get("merchant_col")
+                ).replace(" ", ""):
+                    continue
+            # Remove row from CSV data if all three fields are matched
+            csv_data_copy.remove(csv_row)
+    diff["CSV rows not in DB"] = csv_data_copy
+
+    # Identify DB records not in CSV
+    db_data_copy = copy.deepcopy(db_data)
+    for csv_row in csv_data:
+        for Transaction in db_data_copy:
+            if Transaction.date != csv_row.get(csv_col_mapping.get("date_col")):
+                continue
+            # Convert Transaction decimal to string
+            if str(Transaction.amount) != csv_row.get(
+                csv_col_mapping.get("amount_col")
+            ):
+                continue
+            if "transfer" not in Transaction.type_:
+                # The Huntington CSV sometimes includes a lot of extra spaces
+                if Transaction.merchant.replace(" ", "") != csv_row.get(
+                    csv_col_mapping.get("merchant_col")
+                ).replace(" ", ""):
+                    continue
+            # Remove row from CSV data if all three fields are matched
+            db_data_copy.remove(Transaction)
+    diff["DB records not in CSV"] = db_data_copy
+
+    return diff
 
 
 if __name__ == "__main__":  # pragma: no cover
--- validator/banks/__init__.py
+++
@@ -1,4 +0,0 @@
-import pathlib
-
-APP_ROOT = str(pathlib.Path(__file__).parent.parent.parent)
-FILE_PATH = APP_ROOT + "/validator/banks/real_data/"
