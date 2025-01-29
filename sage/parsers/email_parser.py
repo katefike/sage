70247f597a7b92009b18e3bdd39282fb84878591
Kate Fike <kate.e.fike@gmail.com>
2025-01-28 19:00:39 -0500

    Delete duplicated functions

--- sage/parsers/email_parser.py
+++ sage/parsers/email_parser.py
@@ -144,27 +144,3 @@ def get_bank(body: str) -> str:
                 return bank
     logger.warning("No bank identified")
     return None
-
-
-def transform_amount(raw_amount: str) -> str:
-    """Transform the raw amount into a standardized format"""
-    # Remove the comma
-    raw_amount = re.sub(",", "", raw_amount)
-    # Check for decimals
-    if regex_search(r"(.\d{2})", raw_amount):
-        transformed_amount = raw_amount
-    else:
-        transformed_amount = raw_amount + ".00"
-    return transformed_amount
-
-
-def regex_search(pattern: str, raw_text: str) -> str:
-    """Helper function to perform regex searches with consistent flags"""
-    transformed_text = raw_text.replace("\r", "").replace("\n", " ")
-    match = re.search(pattern, transformed_text, flags=re.DOTALL | re.MULTILINE)
-    if match:
-        if match.group(1):
-            return match.group(1)
-        else:
-            return match.group()
-    return None
