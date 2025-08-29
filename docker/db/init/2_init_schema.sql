7965a5d966601f01245c64785f4299037ef4bd7b
kfike <kate.e.fike@gmail.com>
2025-08-28 22:33:13 -0400

    Remove bank data

--- docker/db/init/2_init_schema.sql
+++ docker/db/init/2_init_schema.sql
@@ -31,16 +31,7 @@ CREATE TABLE IF NOT EXISTS public.banks(
     date_opened DATE,
     date_closed DATE
 );
-INSERT INTO public.banks (name, account, type, email_addresses, date_opened, date_closed)
-VALUES 
-    ('Huntington', 'SAVE', 'liquid', ARRAY['HuntingtonAlerts@email.huntington.com', 'HuntingtonOnline@email.huntington.com'], '2024-01-01', '2024-12-31'),
-    ('Huntington', 'CHECK', 'liquid', ARRAY['HuntingtonAlerts@email.huntington.com', 'HuntingtonOnline@email.huntington.com'], '2024-01-01', '2024-12-31'),
-    ('Huntington', 'CK9706', 'liquid', ARRAY['HuntingtonAlerts@email.huntington.com', 'HuntingtonOnline@email.huntington.com'], '2024-01-01', '2024-12-31'),
-    ('Discover', 'student', 'credit', ARRAY['discover@services.discover.com'], '2024-01-01', '2024-12-31'),
-    ('Discover', 'miles', 'credit', ARRAY['discover@services.discover.com'], '2024-01-01', '2024-12-31'),
-    ('Discover', 'savings', 'liquid', ARRAY['discover@services.discover.com'], '2024-01-01', '2024-12-31');
-INSERT INTO public.banks (name, type, email_addresses, date_opened, date_closed)
-VALUES ('Chase', 'credit', ARRAY['no.reply.alerts@chase.com'], '2024-01-01', '2024-12-31');
+
 CREATE TABLE IF NOT EXISTS public.entity_tags(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     NAME TEXT NOT NULL,
