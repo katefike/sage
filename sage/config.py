d71dfa670adb058be001c346f2b934bdede326fa
kfike <kate.e.fike@gmail.com>
2025-08-28 21:20:35 -0400

    Replace banks_config.yml with DB function call

--- sage/config.py
+++ sage/config.py
@@ -1,23 +1,19 @@
 import os
 import pathlib
-import yaml
 
 from dotenv import dotenv_values
-
+from sage.db.banks import get_banks_config
 
 if os.environ.get("ISDEV") == "True":
     ISDEV = True
     ENV_FILE = "/.env-example"
-    BANKS_CONFIG_FILE = "/banks_config-example.yml"
 else:  # pragma: no cover
     ISDEV = False
     ENV_FILE = "/.env"
-    BANKS_CONFIG_FILE = "/banks_config.yml"
 
 APP_ROOT = str(pathlib.Path(__file__).parent.parent)
 
 ENV = dotenv_values(APP_ROOT + ENV_FILE)
 ENV["ISDEV"] = ISDEV
 
-with open(APP_ROOT + BANKS_CONFIG_FILE, 'r') as file:
-    BANKS_CONFIG = yaml.safe_load(file)
+BANKS_CONFIG = get_banks_config()
