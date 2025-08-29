df2bc65f26095904206f42adedafddd67b6e06e0
kfike <kate.e.fike@gmail.com>
2025-08-29 10:20:20 -0400

    Fix log

--- sage/mx/get_emails.py
+++ sage/mx/get_emails.py
@@ -38,7 +38,7 @@ def main(
             all_email_addresses.update(banks_email_addresses)
             logger.info(
                 f"""
-                Only getting emails from FORWARDING_EMAIL and banks tableemail addresses:
+                Only getting emails from FORWARDING_EMAIL and banks.email_addresses:
                 {all_email_addresses}
                 """
             )
